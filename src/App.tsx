bbe7ada386c36f28e4333b2b5aeebc9b2dca0bee
Minji Choi <comg2843@gmail.com>
2022-07-10 21:29:40 +0300

    remove reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { WeatherInfoContextProvider } from './context/WeatherInfoContext';
 import AppContent from './components/appContent/AppContent';
-import './styles/reset.css';
 import './styles/app.css';
 
 function App() {
