06f69ef0a4ec574e22830e7563618ea4e4760617
Minji Choi <comg2843@gmail.com>
2022-07-10 21:15:47 +0300

    fixed base url

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,4 +1,4 @@
-const URL_BASE = 'http://dataservice.accuweather.com';
+const URL_BASE = 'https://dataservice.accuweather.com';
 export const CITY_ENDPOINT = `${URL_BASE}/locations/v1/cities`;
 export const FORECAST_ENDPOINT = `${URL_BASE}/forecasts/v1/daily/1day`;
 export const ICON_URL = `https://www.accuweather.com/images/weathericons/`;
