e07f4abe47fdb7f5b6a3a827846e7e994ab532f9
Minji Choi <comg2843@gmail.com>
2022-07-10 13:47:09 +0300

    add geolocation button

--- src/components/GeolocationBtn.tsx
+++ src/components/GeolocationBtn.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useWeatherInfo } from '../context/WeatherInfoContext';
+import { CITY_ENDPOINT } from '../utils/consts';
 
 const GeolocationBtn = () => {
+  const { searchCityForecast } = useWeatherInfo();
   const getLocation = () => {
     const onGeoSuccess = (position: GeolocationPosition) => {
       const { latitude, longitude } = position.coords;
+      const geoUrl = `${CITY_ENDPOINT}/geoposition/search?apikey=${process.env.REACT_APP_API_KEY}&q=${latitude},${longitude}`;
       //api call
+      searchCityForecast(geoUrl);
     };
 
     const onGeoError = (err: GeolocationPositionError) => {
