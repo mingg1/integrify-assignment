8df6a7ab73aaafd2a19628c052373c203b82c805
Minji Choi <comg2843@gmail.com>
2022-07-10 13:46:53 +0300

    add city search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { useWeatherInfo } from '../context/WeatherInfoContext';
+import { CITY_ENDPOINT } from '../utils/consts';
 
 const SearchBar = () => {
+  const [city, setCity] = useState('');
+  const { searchCityForecast } = useWeatherInfo();
+
+  const getWeatherByCityName = (evt: FormEvent) => {
+    evt.preventDefault();
+
+    if (city === '') return alert('Write a city name first!');
+
+    const searchUrl = `${CITY_ENDPOINT}/search?apikey=${process.env.REACT_APP_API_KEY}&q=${city}`;
+    searchCityForecast(searchUrl);
+    setCity('');
+  };
+
   return (
-    <form>
-      <input type="text" name="city" id="city" placeholder="Enter city name" />
+    <form onSubmit={getWeatherByCityName}>
+      <input
+        type="text"
+        name="city"
+        id="city"
+        placeholder="Enter city name here"
+        value={city || ''}
+        onChange={(evt) => setCity(evt.target.value)}
+      />
       <button>Get weather info</button>
     </form>
   );
